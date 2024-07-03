a50ea7598ce50013ff0bb7ec664eacc73cc41a04
Courtney Ring <courtneyring3@gmail.com>
2024-07-02 20:56:08 -0400

    start about page

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero/Hero";
+
+const About = () => {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export default About;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,9 @@
 import DOMPurify from 'dompurify'
 
-const Hero = ({text, image}) => {
+const Hero = ({text, image, skinny}) => {
     return (
-        <div className="w-screen h-125 bg-cover bg-left md:bg-center relative flex items-center justify-center px-2" style={{backgroundImage: `url(/images/${image})`}}>
-            <h1 className='text-5xl md:text-7xl md:absolute top-40 right-36 text-white leading-normal text-center' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }}></h1>
+        <div className={`w-screen bg-cover bg-left md:bg-center relative flex items-center justify-center px-2 ${skinny ? 'h-125' : 'h-80'}`} style={{backgroundImage: `url(/images/${image})`}}>
+            { text && <h1 className='text-5xl md:text-7xl md:absolute top-40 right-36 text-white leading-normal text-center' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }}></h1>}
         </div>
     )
 }
