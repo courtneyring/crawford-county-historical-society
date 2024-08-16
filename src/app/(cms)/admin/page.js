ca0a8a17bd38c493ba18504bc7d76d050bac1a98
Courtney Ring <courtneyring3@gmail.com>
2024-08-16 09:07:45 -0400

    start home content

---
+++ src/app/(cms)/admin/page.js
@@ -0,0 +1,28 @@
+import { Hero } from "@/components";
+
+const Admin = () => {
+    const HeroPreview = ({entry}) => (
+        <Hero text={entry.getIn(['data', 'title'])} image=''>
+        </Hero>
+    )
+    return (
+        <html>
+
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="robots" content="noindex" />
+                <title>Content Manager</title>
+            </head>
+
+            <body>
+                <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+                
+
+            </body>
+
+        </html>
+    )
+}
+
+export default Admin;
