eb794f2cae6e0c057befcfc6e38ca217a77a046b
Courtney Ring <courtneyring3@gmail.com>
2024-08-20 16:07:27 -0400

    more home page

--- src/components/ImagePromoBlock/ImagePromoBlock.js
+++ src/components/ImagePromoBlock/ImagePromoBlock.js
@@ -5,10 +5,10 @@ import Promo from "../Promo/Promo";
 const ImagePromoBlock = ({header, body, cta, image, reverse}) => {
 
     return (
-        <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-5 py-10">
+        <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 ">
             
             <img src={image} className={!reverse && `md:order-2`} />
-            <div className="flex flex-col items-start justify-start md:order-1 md:justify-center">
+            <div className={`flex flex-col items-start justify-start md:order-1 md:justify-center ${reverse ? 'md:pl-10' : 'md:pr-10'}`}>
                 <h3 className="text-5xl mb-5">{header}</h3>
                 <p className="text-xl mb-5">{body}</p>
                 <Button url={cta.value}>{cta.label}</Button>
