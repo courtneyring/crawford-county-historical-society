c97fb91a3caed827bd678dc172c93080ebfff6b1
Courtney Ring <courtneyring3@gmail.com>
2024-08-20 16:03:20 -0400

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,15 @@ export default function Home() {
   return (
     <main className="flex  flex-col items-center ">
       <Hero text={hero.title} image={hero.image} cta={{label: 'Learn More', value: '/about'}}/>
-      <div className='container'>
-        <p>The Crawford County Historical Society and Museum is a non-governmental, 501c3 non-profit corporation, dedicated to “Collecting, Preserving and Interpreting Documents and Artifacts that illustrate the history of Crawford County, Michigan”.</p>
+      <div className="bg-lightgrey w-full">
+        <div className='container mx-auto'>
+          <p className="text-4xl leading-10 max-w-5xl py-20 mx-auto">The Crawford County Historical Society and Museum is a non-governmental, 501c3 non-profit corporation, dedicated to “Collecting, Preserving and Interpreting Documents and Artifacts that illustrate the history of Crawford County, Michigan”.</p>
+        </div>
+      </div>
+
+      <div className="container">
         {imagePromos.map((promo, idx) => {
-          return <ImagePromoBlock heading={promo.heading} body={promo.body} cta={promo.cta} image={promo.image} key={`promo-${idx}`}/>
+          return <ImagePromoBlock heading={promo.heading} body={promo.body} cta={promo.cta} image={promo.image} key={`promo-${idx}`} reverse={idx%2}/>
         })}
 
 
