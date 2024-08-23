cd140697364ad2aaf8ab52aebde874597c56608a
Courtney Ring <courtneyring3@gmail.com>
2024-08-23 12:32:52 -0400

    gallery

--- src/app/(pages)/gallery/page.js
+++ src/app/(pages)/gallery/page.js
@@ -2,17 +2,19 @@
 
 import Hero from "@/components/Hero/Hero";
 import { attributes } from '../../../copy/gallery/copy.md'
+import { useMemo } from "react";
+import ImageGrid from "@/components/ImageGrid/ImageGrid";
 
 const Gallery = () => {
-    let { heroImage, pageTitle } = attributes;
-
+    let { heroImage, pageTitle, images } = attributes;
+    let imageArr = useMemo(() => images.map((image) => image.image), images)
 
     return (
         <main className="flex flex-col items-center">
             <Hero skinny image={heroImage} />
             <div className='max-w-7xl w-full px-2'>
                 <h1 className="my-10">{pageTitle}</h1>
-
+                <ImageGrid images={imageArr} />
             </div>
 
         </main>
