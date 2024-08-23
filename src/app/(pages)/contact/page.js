c8992fbcf6d99f5abec0cf828be4b32fc0bf6edf
Courtney Ring <courtneyring3@gmail.com>
2024-08-23 14:24:09 -0400

    contact page

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -2,6 +2,7 @@
 
 import Hero from "@/components/Hero/Hero";
 import { attributes } from '../../../copy/contact/copy.md'
+import { Map } from "@/components";
 
 const About = () => {
     let { heroImage, pageTitle, address, phone, email, hours } = attributes;
@@ -11,7 +12,44 @@ const About = () => {
             <Hero skinny image={heroImage} />
             <div className='max-w-7xl w-full px-2'>
                 <h1 className="my-10">{pageTitle}</h1>
-                <p></p>
+                <div className="grid grid-cols-2">
+                    <div>
+                        <p>{address.street}
+                            <br />
+                            {address.street2}
+                            <br />
+                            {address.city}, {address.state} {address.zip}
+                        </p>
+                        <p>
+                            <span className="font-bold">Phone</span><br />
+                            {phone}
+                        </p>
+                        <p>
+                            <span className="font-bold">Email</span><br />
+                            {email}
+                        </p>
+                        <p>
+                            <span className="font-bold">Hours</span><br />
+                            <div className="grid grid-cols-2 max-w-52">
+                                {Object.entries(hours).map(([key, value]) => {
+                                    return (
+                                        <>
+                                            <span className="capitalize">{key}</span>
+                                            <span>{value}</span>
+                                        </>
+                                    )
+
+                                })}
+                            </div>
+                        </p>
+
+
+                    </div>
+                    <Map />
+
+                </div>
+
+                
             </div>
 
         </main>
