6870cc0e1299016c748da74b840ecafffe27083b
Courtney Ring <courtneyring3@gmail.com>
2024-08-23 13:52:58 -0400

    update contact schema

--- src/app/(pages)/contact/page.js
+++ src/app/(pages)/contact/page.js
@@ -1,18 +1,17 @@
 'use client';
 
 import Hero from "@/components/Hero/Hero";
-import { attributes, react as AboutContent } from '../../../copy/about/copy.md'
+import { attributes } from '../../../copy/contact/copy.md'
 
 const About = () => {
-    let { heroImage, pageTitle, body } = attributes;
-    console.log(attributes)
+    let { heroImage, pageTitle } = attributes;
 
     return (
         <main className="flex flex-col items-center">
             <Hero skinny image={heroImage} />
             <div className='max-w-7xl w-full px-2'>
                 <h1 className="my-10">{pageTitle}</h1>
-                <p className="text-xl leading-8 mx-auto"><AboutContent /></p>
+                <p></p>
             </div>
 
         </main>
