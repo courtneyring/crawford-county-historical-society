0a77b1833970698e760b0d9e8d57fb10149c85ac
Courtney Ring <courtneyring3@gmail.com>
2024-09-25 17:09:43 -0400

    fix config for staging url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
     output: 'export',
+    basePath: process.env.NODE_ENV == 'development' ? '' : '/crawford-county-historical-society',
+    assetPrefix: process.env.NODE_ENV == 'development' ? '' : '/crawford-county-historical-society',
     webpack: (cfg) => {
         cfg.module.rules.push(
             {
