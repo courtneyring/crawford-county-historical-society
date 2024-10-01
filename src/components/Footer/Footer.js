6214f974b8b5df30af46c2aeb77d9bd9433066ec
Courtney Ring <courtneyring3@gmail.com>
2024-10-01 12:23:24 -0400

    fix errors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = ({ address, links, hours, logoImg }) => {
       <footer
         className={`flex flex-col s bg-black items-center text-surface text-white font-light w-full ${styles.footer}`}>
         <div className="container w-full py-10 px-5">
-          <div className={styles.grid-container}>
+          <div className={styles['grid-container']}>
             <div className={styles['image-body']}>
               <img src={imagePath(logoImg)} className='w-52 md:w-44 mb-5 mx-auto md:mx-0'/>
               <Address address={address}/>
--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from "react";
 import styles from './Overlay.module.scss';
 import { imagePath } from "@/utils/imagePrefix";
@@ -14,7 +16,7 @@ const Overlay = ({ children, closeFn, hideClose, background }) => {
 
     return (
         <>
-            <div className={`w-screen h-screen  bg-translucent p-6 absolute top-0 left-0 ${styles.overlay-container} ${fadeOut && styles.fadeOut}`}>
+            <div className={`w-screen h-screen  bg-translucent p-6 absolute top-0 left-0 ${styles['overlay-container']} ${fadeOut && styles.fadeOut}`}>
             </div>
             <div className={`${background ?? bg-lightgrey} p-5 absolute ${styles.overlay-dialog} ${fadeOut ? styles.fadeOut : styles.fadeIn}`}>
                 {!hideClose && <div className="absolute top-5 right-5 w-7 cursor-pointer" onClick={close} >
